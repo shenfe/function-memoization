6e292366ac7db4bd1f7af60e290c53706144f1ef
hengwu <youchuangqiba@163.com>
2017-11-15 00:05:59 +0800

    upd8: readme.

--- README.md
+++ README.md
@@ -34,5 +34,61 @@ cacheMethod(obj, 'method', { async: true });
 obj.method(/* args */);
 ```
 
+## Example
+
+```js
+const { cacheFunction, cacheMethod } = require('../src/index.js');
+
+// Define function1
+let testFunction = (p1, p2, p3, callback) => {
+    console.log('function1 begins');
+    let arr = ['function1', p1, p2.n, p3[0]];
+    setTimeout(function () {
+        callback(arr);
+    }, 1000);
+};
+
+// Define function2 as a method of an object
+const obj = {
+    testMethod: function (p1, p2, p3, callback) {
+        console.log('function2 begins');
+        let arr = ['function2', p1, p2.n, p3[0]];
+        setTimeout(function () {
+            callback(arr);
+        }, 1000);
+    }
+};
+
+// Cache function1, keeping the callback "async"
+testFunction = cacheFunction(testFunction, {
+    async: true
+});
+
+// Cache function2
+cacheMethod(obj, 'testMethod');
+
+// Execute function1 1st time
+testFunction(1, { n: 2 }, [3], function (res) {
+    console.log(res);
+});
+
+// Execute function2 1st time
+obj.testMethod(4, { n: 5 }, [6], function (res) {
+    console.log(res);
+});
+
+setTimeout(function () {
+    // Execute function1 2nd time
+    testFunction(1, { n: 2 }, [3], function (res) {
+        console.log(res);
+    });
+
+    // Execute function2 2nd time
+    obj.testMethod(4, { n: 5 }, [6], function (res) {
+        console.log(res);
+    });
+}, 1000);
+```
+
 ## License
 MIT
--- test/index.js
+++ test/index.js
@@ -1,51 +1,51 @@
-// Import
 const { cacheFunction, cacheMethod } = require('../src/index.js');
 
 // Define function1
-let testFunc1 = (p1, p2, p3, callback) => {
-    console.log('func1 begins');
-    let arr = ['func1', p1, p2.n, p3[0]];
+let testFunction = (p1, p2, p3, callback) => {
+    console.log('function1 begins');
+    let arr = ['function1', p1, p2.n, p3[0]];
     setTimeout(function () {
         callback(arr);
     }, 1000);
 };
 
-// Define function2
-const testFunc2 = ;
-
-// Define an object and a method
+// Define function2 as a method of an object
 const obj = {
     testMethod: function (p1, p2, p3, callback) {
-        console.log('func2 begins');
-        let arr = ['func2', p1, p2.n, p3[0]];
+        console.log('function2 begins');
+        let arr = ['function2', p1, p2.n, p3[0]];
         setTimeout(function () {
             callback(arr);
         }, 1000);
     }
 };
 
-// Cache function1
-testFunc1 = cacheFunction(testFunc1, {
+// Cache function1, keeping the callback "async"
+testFunction = cacheFunction(testFunction, {
     async: true
 });
 
-// Cache f
-cacheMethod(obj, 'testFunc2');
+// Cache function2
+cacheMethod(obj, 'testMethod');
 
-testFunc1(1, { n: 2 }, [3], function (res) {
+// Execute function1 1st time
+testFunction(1, { n: 2 }, [3], function (res) {
     console.log(res);
 });
 
-obj.testFunc2(4, { n: 5 }, [6], function (res) {
+// Execute function2 1st time
+obj.testMethod(4, { n: 5 }, [6], function (res) {
     console.log(res);
 });
 
 setTimeout(function () {
-    testFunc1(1, { n: 2 }, [3], function (res) {
+    // Execute function1 2nd time
+    testFunction(1, { n: 2 }, [3], function (res) {
         console.log(res);
     });
 
-    obj.testFunc2(4, { n: 5 }, [6], function (res) {
+    // Execute function2 2nd time
+    obj.testMethod(4, { n: 5 }, [6], function (res) {
         console.log(res);
     });
 }, 1000);
